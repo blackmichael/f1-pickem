caa2dd00cfcc2fd6da696422176723f548ecffb0
Michael Black <michaelpblack16@gmail.com>
2022-04-17 23:41:23 -0400

    Refactored nav bar to be modern / mobile-friendly (#27)

---
+++ web/src/components/common/NavLink.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ListItem, Typography, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default function NavLink(props) {
+  const { icon, primary, to } = props;
+  const isMobile = window.innerWidth <= 500;
+
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef((itemProps, ref) => (
+        <Link to={to} ref={ref} {...itemProps} />
+      )),
+    [to]
+  );
+
+  return (
+    <Grid item>
+      <ListItem 
+        button 
+        disableGutters={isMobile} 
+        style={{paddingLeft: "16px"}}
+        component={renderLink} 
+    >
+        {icon}
+        <Typography variant="body1">
+          {primary}
+        </Typography>
+      </ListItem>
+    </Grid>
+  );
+}
