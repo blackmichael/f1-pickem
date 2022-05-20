89e0c98aef91b6d9eb11b620976a682287fd2ebd
Michael Black <michaelpblack16@gmail.com>
2022-05-19 21:17:29 -0700

    Updated get races lambda (and formated things) (#33)

--- backend/pkg/domain/races.go
+++ backend/pkg/domain/races.go
@@ -6,12 +6,17 @@ import (
 
 type Race struct {
 	// Partition Key
-	Season     string    `dynamodbav:"Season" json:"season"`
+	Season string `dynamodbav:"Season" json:"season"`
 	// Sort Key
 	RaceNumber string    `dynamodbav:"RaceNumber" json:"race_number"`
+	RaceId     string    `dynamodbav:"RaceID" json:"id"`
 	RaceName   string    `dynamodbav:"RaceName" json:"race_name"`
 	RaceDate   string    `dynamodbav:"RaceDate" json:"race_date"`
 	StartTime  time.Time `dynamodbav:"StartTime" json:"start_time"`
 }
 
 type Races []*Race
+
+func GetRaceId(season, raceNumber string) string {
+	return season + raceNumber
+}
