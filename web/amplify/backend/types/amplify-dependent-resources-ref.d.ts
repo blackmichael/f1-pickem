b1abc15687916b394259d5b435a5d2546522e6f9
Michael Black <michaelpblack16@gmail.com>
2023-01-21 12:22:28 -0500

    Added amplify backend

---
+++ web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "f1pickemd34e858d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
